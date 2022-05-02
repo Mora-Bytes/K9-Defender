e6730ec3a2ad495ec8a3b6def7775eebde532a48
K9-Software <99689410+K9-Software@users.noreply.github.com>
2022-05-02 06:48:10 +0200

    Delete code directory

--- code/secui.ps1
+++
@@ -1,9 +0,0 @@
-﻿$title = "K9-Defender"
-$host.ui.rawui.WindowTitle = $title
-cmd /c "color 1E"
-Write-Progress -Activity "Scanning.." -Status "Scanning For Malware" -Id 1 -PercentComplete 99
-cmd /c "start /wait scanfile"
-echo done.
-Write-Progress -Activity "loading..." -Status "loading antispyware" -Id 1 -PercentComplete 100
-cmd /c "start K9-Defender"
-echo done.
